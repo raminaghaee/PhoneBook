616299f1d56be67f79a0cd1da3c98531b314e439
raminaghaee <rn.aghaee@gmail.com>
2025-10-31 19:37:14 +0330

    Add DbContext and ORM Pakage "EntityFramework 6.5.1"

---
+++ PhoneBook/PhoneBookContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace PhoneBook
+{
+    // DbContext
+    public class PhoneBookContext : DbContext
+    {
+        #region Ctor
+        public PhoneBookContext() : base("name=PhoneBookConnection")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<PhoneBookContext>());
+        } 
+        #endregion
+
+    }
+}
